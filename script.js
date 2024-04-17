336ec2232392ed47e3ea4bc4029b7f772ec92995
Kauahp00 <132684345+Kauahp00@users.noreply.github.com>
2024-04-17 12:11:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.key === 'q') {
+        document.getElementById('Sobre').click();
+    }
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.key === "i") {
+        window.open("https://futsport.com.br/wp-content/uploads/2024/04/felipe-anderson.jpg");
+        }
+
+
+
+    if (event.ctrlKey && event.key === "m") {
+    document.getElementById("mensagem").focus();
+    }
+    });
